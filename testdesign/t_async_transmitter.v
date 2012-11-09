e7a89f299acac763d81239d3f10450c1228a0884
greedgenius <greedgenius@greedhome.(none)>
2012-11-09 22:46:43 +0800

    made t_async_transmitter more readable

--- testdesign/t_async_transmitter.v
+++ testdesign/t_async_transmitter.v
@@ -1,7 +1,9 @@
 `timescale 1ns / 1ps
+
 module t_async_transmitter;
-    
-    parameter CLK_CYCLE=10;
+
+	parameter CLK_CYCLE=10;
+	parameter UART_TX_WAIT = 500000000/19200;
 
 	// Inputs
         reg		clk,TxD_start, rst_n;
@@ -9,109 +11,89 @@ module t_async_transmitter;
 
 	// Outputs
         wire		TxD, TxD_busy;
-        integer		i;
+        
+	// Integers
+	integer		i;
+	
+	
 	// Instantiate the Unit Under Test (UUT)
 	async_transmitter async_transmitter(clk, TxD_start, TxD_data, TxD, TxD_busy, rst_n);
 	
-    task randin; begin
-        TxD_data=$random;
+	task sendTx; begin
+        	TxD_data=$random;
+		@ (posedge clk); TxD_start = 1;
+		@ (posedge clk); TxD_start = 0;
 
-        @ (posedge clk); 
-        @ (posedge clk); TxD_start = 1;
-        @ (posedge clk); TxD_start = 0;
+		for (i=0;i<50000000/1500;i=i+1)
+		begin
+			@ (posedge clk); 
+		end
+	end
+	endtask
 
-        for (i=0;i<50000000/1500;i=i+1)
-        begin
-        @ (posedge clk); 
-        end
-    end
-    endtask
-
-    task reset; begin
-    #10; rst_n=0;
-    #10; rst_n=1;
-    end
-    endtask
+	task reset; begin
+		#10; rst_n=0;
+		#10; rst_n=1;
+	end
+	endtask
     
     
-always @ (posedge TxD_start)
-        uart_decoder;
+	always @ (posedge TxD_start)
+		uart_decoder;
     
-parameter UART_TX_WAIT = 500000000/19200;
 
-task uart_decoder;
+	task uart_decoder;
 	integer i;
 	reg [7:0] tx_byte;
 	reg tx_parity;
 	begin
-        // Wait for start bit
-        while (TxD == 1'b1)
-        @(TxD);
-
-        #(UART_TX_WAIT + (UART_TX_WAIT/2));
-
-        for ( i = 0; i < 8 ; i = i + 1 ) begin
-            tx_byte[i] = TxD;
-            #UART_TX_WAIT;
-        end
-
-	tx_parity=TxD;
-	#UART_TX_WAIT;
-
-        //Check for stop bit
-        if (TxD == 1'b0) begin
-            //$display("* WARNING: user stop bit not received when expected at time %d__", $time);
-            // Wait for return to idle
-            while (TxD == 1'b0)
-            @(TxD);
-            //$display("* USER UART returned to idle at time %d",$time);
-        end
-        // display the char
-            $write("%h\t%d\t%d\n", tx_byte,tx_parity,~^tx_byte);
-    end
-endtask
+	        // Wait for start bit
+	        while (TxD == 1'b1)
+			@(TxD);
+			#(UART_TX_WAIT + (UART_TX_WAIT/2));
+	
+	        for ( i = 0; i < 8 ; i = i + 1 ) begin
+	        	tx_byte[i] = TxD;
+	        	#UART_TX_WAIT;
+	        end
+		
+		tx_parity=TxD;
+		#UART_TX_WAIT;
+	
+	        //Check for stop bit
+	        if (TxD == 1'b0) begin
+	        	//$display("* WARNING: user stop bit not received when expected at time %d__", $time);
+	        	// Wait for return to idle
+	        	while (TxD == 1'b0)
+	        	@(TxD);
+	        	//$display("* USER UART returned to idle at time %d",$time);
+	        end
+	        // display the char
+	        $write("receive byte =%h\tparity=%d\texpected=%d", tx_byte,tx_parity,~^tx_byte);
+		if (tx_parity!= ~^tx_byte) $write("\t parity error!!!!!!\n");
+		else $write("\n");
+	end
+	endtask
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    initial
-     begin
-     $dumpfile("async_transmitter.vcd");
-     $dumpvars(0, async_transmitter);
-     end
-
-
 	initial begin
-	// Initialize Inputs
-		clk=0;
-	// Add stimulus here
-	reset;
-        #10;
-        randin; randin; randin; randin; randin; randin; randin; randin; randin; randin; randin;
-        randin; randin; randin; randin; randin; randin; randin; randin; randin; randin; randin;
-        randin; randin; randin; randin; randin; randin; randin; randin; randin; randin; randin;
-        randin; randin; randin; randin; randin; randin; randin; randin; randin; randin; randin;
-        #10; $finish;
-
-
+		$dumpfile("async_transmitter.vcd");
+		$dumpvars(0, async_transmitter);
 	end
 
 
-    always #(CLK_CYCLE/2) clk=~clk;
-    
+	//initial begin
+	//	sendTx;
+        //#10; $finish;
+	//end
+	initial begin
+		repeat(20) sendTx;
+		$finish;
+	end
 
-      
+	always #(CLK_CYCLE/2) clk=~clk;
+	initial begin
+		clk=0;
+		reset;
+	end
 endmodule
 
